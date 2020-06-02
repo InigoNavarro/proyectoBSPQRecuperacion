d269ebbbf17dbc4133529185f51e1404d9cedd35
Ditto <inigonavarro@opendeusto.es>
2020-06-02 10:37:39 +0200

    base de la pagina principal, no funciones

---
+++ src/main/java/es/deusto/SPQ/Interfaces/Controllers/controllerPagPrincipal.java
@@ -0,0 +1,49 @@
+package es.deusto.SPQ.Interfaces.Controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class controllerPagPrincipal {
+
+    @FXML
+    private Button botonCatalogo;
+
+    @FXML
+    private Button botonGrafico;
+
+    @FXML
+    private Button botonReservas;
+
+    @FXML
+    private Button botonCliente;
+
+    @FXML
+    private Button botonJuegos;
+
+    @FXML
+    void irVentanaCatalogo(ActionEvent event) {
+
+    }
+
+    @FXML
+    void irVentanaCliente(ActionEvent event) {
+
+    }
+
+    @FXML
+    void irVentanaGrafico(ActionEvent event) {
+
+    }
+
+    @FXML
+    void irVentanaJuegos(ActionEvent event) {
+
+    }
+
+    @FXML
+    void irVentanareservas(ActionEvent event) {
+
+    }
+
+}
