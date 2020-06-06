bafb239d63e7ef77f127f57290e957cf34ea2dbe
Ditto <inigonavarro@opendeusto.es>
2020-06-06 17:53:09 +0200

    Coveralls funciona?

--- src/test/java/es/deusto/SPQ/BD/testGestores/GestorJuegoTest.java
+++ src/test/java/es/deusto/SPQ/BD/testGestores/GestorJuegoTest.java
@@ -1,17 +1,46 @@
 package es.deusto.SPQ.BD.testGestores;
 
+import static org.junit.Assert.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.List;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import es.deusto.SPQ.CrearDatosBD;
+import es.deusto.SPQ.BD.Gestores.GestorJuego;
+import es.deusto.SPQ.BD.Objetos.Juego;
+
 class GestorJuegoTest {
 
   @BeforeEach
-  void setUp() throws Exception {}
+  void setUp() throws Exception {
+	  CrearDatosBD cdBD = new CrearDatosBD();
+	  cdBD.cargarBase();
+  }
 
   @Test
-  void test() {
-    fail("Not yet implemented");
+  void obtenerTodosLosJuegos() {
+	  List<Juego> lista;
+	  lista = GestorJuego.getTodosJuegos();
+	  assertNotEquals(0, lista.size());
+  }
+  
+  @Test
+  void obtenerUnJuego() {
+	  assertEquals("Transistor", GestorJuego.selectJuego("Transistor").getNombreJuego());
+  }
+  
+  @Test
+  void eliminarUnJuego() {
+	  GestorJuego.borrarJuego("Transistor");
+	  assertNull(GestorJuego.selectJuego("Transistor"));  
+  }
+  
+  @Test
+  void actualizarUnJuego() {
+	  GestorJuego.updateJuego("Bastion", 10, "ERP", 32, true, "Bastion");
+	  assertNotEquals(10, GestorJuego.selectJuego("Bastion").getPrecio());  
   }
 }
